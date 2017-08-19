748a79508424d1628e3a63367c69e0eb7fe788c3
bengrunfeld <benjamin.grunfeld@gmail.com>
2017-08-19 12:45:03 -0700

    Add reselect dependency

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { Map } from 'immutable'
+import './App.css'
 
 class App extends Component {
   render() {
+    const obj = {a: 1, b: 2, c: 3, d: 4}
+    const map1 = Map(obj)
+    
     return (
       <div className="App">
-        <h1>Hello</h1>
+        <h1>Maps</h1>
+        <ul>
+          <li>{map1.get('a')}</li>
+          <li>{map1.get('b')}</li>
+          <li>{map1.get('c')}</li>
+          <li>{map1.get('d')}</li>
+        </ul>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
