b8623226272c6cd7c2c61ccda9e44a739f8641a3
bengrunfeld <benjamin.grunfeld@gmail.com>
2017-08-20 18:47:45 -0700

    Place App component in own directory

--- src/components/main/App.js
+++ src/components/main/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import { Provider } from 'react-redux'
-import configureStore from './app/state/store'
+import configureStore from '../../app/state/store'
 
+import './reset.css'
 import './App.css'
 
 const initialState = {}
